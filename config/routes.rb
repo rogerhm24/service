b33dc7fa3b954c95044dac569105bca60f1f81bf
Roger Mais <rogerhm24@gmail.com>
2017-01-13 22:15:48 +0000

    login file generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts # shortcut to make diffrent routes for contsct.. eg contact page route
   get '/about' => 'pages#about'
   root 'pages#home'
