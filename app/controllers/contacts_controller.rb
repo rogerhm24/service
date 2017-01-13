d8f59d2ef6e423f252704ac8ee9a78ba59cf203e
Roger Mais <rogerhm24@gmail.com>
2017-01-13 19:05:29 +0000

    validation of fomr

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,27 @@
 class ContactsController < ApplicationController
-    def new
+    def new # new is the name that matches with  " new " in contacts folder 
     
         @contact = Contact.new  # @contact is a variable 
         
     
     end    
     
-    def create
+    def create # this handle form submition  to get info from form
     
+        @contact = Contact.new(contact_parameters)
     
+        if @contact.save
+            flash[:success] = 'Message Sent!' # success is a key that will be sent to flassh message in application.html.erb
+            redirect_to new_contact_path # if successfull sent redirect back to form page with "message sent"
+        else
+            flash[:danger] = 'Error,Message not saved' # error is a key that will be sent to flassh message in application.html.erb
+            redirect_to new_contact_path # if successfull sent redirect back to form page with " ERROR"
+        
+        end    
     end
-
+    
+    private
+        def contact_parameters
+            params.require(:contact).permit(:name,:email,:comments) #this is a security feature so people cant see in url line
+        end
 end    
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact <ActiveRecord::Base
     
-    
+    validates :name,presence: true  # checks to see if the name field is validated 
+    validates :email,presence: true # checks to see if the email field is validated 
 
 end
