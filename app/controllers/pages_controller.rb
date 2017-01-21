7abb7daa9b45a915c2f634314d61905d0b5f7291
Roger Mais <rogerhm24@gmail.com>
2017-01-21 19:35:36 +0000

    added plan variables to sin ip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def home
+      @basic_plan =Plan.find(1) #Allow to get the Plan with this id ( eg BASIC)
+      @pro_plan = Plan.find(2) #Allow to get the Plan with this id ( eg PRO)
     end
     
     def about
