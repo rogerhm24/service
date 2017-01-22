cb2211efb194121239309f6da07246baaae91712
Roger Mais <rogerhm24@gmail.com>
2017-01-22 17:40:50 +0000

    added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,9 @@
 ####app/controllers/users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController #creating a class and inhertiting devise class
   
+  before_filter :select_plan, only: :new # select_plan is he name of filter ,that will only happen when user goes to sign up pages ( new ) 
+  
+  
   def create #adding to the devise "create" action so we can create more than jus user name, ect.
     super do |resource|
       if params[:plan] 
@@ -14,5 +17,12 @@ class Users::RegistrationsController < Devise::RegistrationsController #creating
     end
   end
   
+  private
+  def select_plan  # method select_plan ( FOR a case if user enters 3 or any otehr number in url brower line)
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2') # unless plan exist and plan is either 1 or 2 ,if not 
+    flash[:notice] = "Please select a membership plan to sign up." # redirec to root page with this message
+    redirect_to root_url # redirect to root page
+    end
+  end
 end
 
