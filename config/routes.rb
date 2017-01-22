b634d1589057391102c00616e312cb7b293d569f
Roger Mais <rogerhm24@gmail.com>
2017-01-22 16:43:42 +0000

    updated route and fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users   # shortcut to make diffrent routes for user authentication ( login).. eg login route
+                      #updated route to add new route for users ( see/controller/users/ registartion_controller.rb)
+  devise_for :users, controllers:{registrations: 'users/registrations'}   # shortcut to make diffrent routes for user authentication ( login).. eg login route
   resources :contacts # shortcut to make diffrent routes for contacts ( comments).. eg contact page route
   get '/about' => 'pages#about'
   root 'pages#home'
